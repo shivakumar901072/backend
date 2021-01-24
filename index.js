b1e991412bf651e6aebfe7ceb44f03db99c26de6
shivakumar901072 <shivakumar901072@gmail.com>
2021-01-24 12:11:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+//import routes
+const authRoute = require('./routes/auth');
+
+
+dotenv.config();
+
+//connect to db
+mongoose.connect(
+ process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true },
+   () =>   console.log('connected to db')
+  );
+
+//Middleware
+app.use(express.json());
+
+//Route Middlewares
+app.use('/api/user', authRoute);
+
+app.listen(8000, () => console.log('Server up and running'));
---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const User = require('../model/User');
+const {registerValidation} = require('../validation');
+
+
+
+router.post('/register', async(req,res) => {
+    //lets validate the data before  a user
+
+    // const { error}= Joi.validate(req.body, schema);
+     const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);  
+    }catch(err){
+        res.status(400).send(err);
+    }
+
+});
+
+
+module.exports = router;
